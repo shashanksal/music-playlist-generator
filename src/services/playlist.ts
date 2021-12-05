efea7fbaa739e9d69e9d2c2bd380cfab88568b31
Shashank Salunkhe <shashank.sal047@gmail.com>
2021-12-05 11:58:31 +1100

    feat: add userDisconnect handler function

--- src/services/playlist.ts
+++ src/services/playlist.ts
@@ -21,10 +21,24 @@ const getCurrentPlaylist = async () => {
 const shufflePlaylist = playlist => {
 	if (!playlist?.length) {
 		logger.error("No playlist to shuffle");
+		throw new Error("No playlist to shuffle");
 	}
 	const shuffledLikes = shuffle(playlist.likes);
 	playlist.likes = shuffledLikes;
 	return playlist;
 };
 
-export { addToPlaylist, getCurrentPlaylist, shufflePlaylist };
+const removeFromPlaylist = async likes => {
+	logger.debug("Invoked removeFromPlaylist");
+	if (!likes?.length) {
+		logger.error("No playlist to remove");
+		throw new Error("No playlist to remove");
+	}
+	const currentPlaylist = await getCurrentPlaylist(); //get current playlist
+	const currentLikes = currentPlaylist.map(playlist => playlist.likes);
+	const likesIds = likes.map(like => like.id);
+	const filteredLikes = currentLikes.filter(currentLike => !likesIds.includes(currentLike.id));
+	return await addRecord(filteredLikes);
+};
+
+export { addToPlaylist, getCurrentPlaylist, shufflePlaylist, removeFromPlaylist };
